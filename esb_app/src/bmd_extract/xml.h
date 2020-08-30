a40ed9f1261d05ae8fedee8ee791a2152ee84fa0
b pavan <bpavan1106@gmail.com>
2020-08-30 15:35:42 +0530

    added bmd_extract

--- esb_app/src/bmd_extract/xml.h
+++ esb_app/src/bmd_extract/xml.h
@@ -1,27 +1,3 @@
-/*
-typedef struct bmdMessage_
-{
-    char   * Message;
-    char  *Sender;
-    char * Destination;
-    char  *MessageID;
-    char  *MessageType;
-    char  *Signature;
-    char  *ReferenceID;
-}bmdMessage;
-
-bmdMessage bm ;
-enum bmd
-{
-    Message=0,
-    Sender=1,
-    Destination=2,
-    MessageID=3,
-    MessageType=4,
-    Signature=5,
-    ReferenceID=6,
-};
-*/
 typedef struct bmdMessage_
 {
     char   * Message;
