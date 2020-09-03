bd8e2e2d7e25849bfd38ebd4d058aed48a4fdc35
b pavan <bpavan1106@gmail.com>
2020-09-03 14:56:38 +0530

    add new skeleton

--- esb_app/src/esb/esb.h
+++ esb_app/src/esb/esb.h
@@ -1,39 +1,13 @@
 int process_esb_request(char* bmd_file_path);
 
+typedef struct {
+    char* sender_id;
+    char* destination_id;
+    char* message_type;
+    // TODO: Other fields
+} bmd_envelop;
 
-
-
-typedef struct 
-{
-    char  *Sender;
-    char * Destination;
-    char  *MessageID;
-    char  *MessageType;
-    char *CreationDateTime;
-    char  *Signature;
-    char  *ReferenceID;
-}bmd_envelope;
-
-
-typedef struct 
-{
-  bmd_envelope be;
-  char * Payload;
+typedef struct {
+    bmd_envelop envelop;
+    char* payload;
 } bmd;
-
-static bmd bm;
-
-
-extern bmd_envelope  extract_envelope(char * filepath);
-extern char * extract_payload(char * filepath);
-
-const char * attributes[] =  {
-    "MessageID",
-    "MessageType",
-    "Sender",
-    "Destination",
-    "CreationDateTime",
-    "Signature",
-    "ReferenceID"
-};
- 
