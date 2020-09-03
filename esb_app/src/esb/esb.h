f7683ababf75393fb901c5820d2bf33300f454c5
b pavan <bpavan1106@gmail.com>
2020-09-03 11:24:21 +0530

    added esb.h

--- esb_app/src/esb/esb.h
+++ esb_app/src/esb/esb.h
@@ -1,1 +1,39 @@
 int process_esb_request(char* bmd_file_path);
+
+
+
+
+typedef struct 
+{
+    char  *Sender;
+    char * Destination;
+    char  *MessageID;
+    char  *MessageType;
+    char *CreationDateTime;
+    char  *Signature;
+    char  *ReferenceID;
+}bmd_envelope;
+
+
+typedef struct 
+{
+  bmd_envelope be;
+  char * Payload;
+} bmd;
+
+static bmd bm;
+
+
+extern bmd_envelope  extract_envelope(char * filepath);
+extern char * extract_payload(char * filepath);
+
+const char * attributes[] =  {
+    "MessageID",
+    "MessageType",
+    "Sender",
+    "Destination",
+    "CreationDateTime",
+    "Signature",
+    "ReferenceID"
+};
+ 
